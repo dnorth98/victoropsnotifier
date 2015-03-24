51628f69b0620503604b63107c06614af409e372
Dave North <dnorth98@gmail.com>
2015-03-24 15:58:14 +0000

    exception handling

--- src/Signiant/VictorOps/Notifier.php
+++ src/Signiant/VictorOps/Notifier.php
@@ -13,15 +13,21 @@ class Notifier implements Transport
     public function __construct($config)
     {
 	$routing_key = "everyone";
-			
-	if (array_key_exists("routing_key",$config))
-	{
-		$routing_key = $config['routing_key'];
-	}
 
-	if (!array_key_exists("endpoint_url",$config))
+	if (is_array($config))
+	{			
+		if (array_key_exists("routing_key",$config))
+		{
+			$routing_key = $config['routing_key'];
+		}
+
+		if (!array_key_exists("endpoint_url",$config))
+		{
+			throw (new MissingEndpointURLException('Config must contain an endpoint_url entry',1));
+		}
+	} else
 	{
-		throw (new MissingEndpointURLException());
+		throw (new MissingEndpointURLException('Config must be an array',2));
 	}
 	
 	$endpointURL = $config['endpoint_url'] . "/" . $routing_key;
